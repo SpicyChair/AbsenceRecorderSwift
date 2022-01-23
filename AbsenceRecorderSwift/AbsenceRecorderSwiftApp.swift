d23d27f33cf09d03d7e19b807e64a9d1bf814809
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-01-23 17:53:15 +0000

    Initial Commit

---
+++ AbsenceRecorderSwift/AbsenceRecorderSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderSwiftApp.swift
+//  AbsenceRecorderSwift
+//
+//  Created by Hin, Ethan-Scott (WING) on 23/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
