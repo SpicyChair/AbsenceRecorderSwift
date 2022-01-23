5e7562e616ad9797bdb40304bf61ce626ff5d38f
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-01-23 18:27:11 +0000

    add file

--- AbsenceRecorderSwift/Model/Division.swift
+++ AbsenceRecorderSwift/Model/Division.swift
@@ -10,9 +10,22 @@ import Foundation
 
 class Division {
     let code: String
-    let students:[String] = []
+    var students:[Student] = []
     
     init(code: String) {
         self.code = code
     }
+    
+    #if DEBUG
+    static func createDivision(code: String, of size: Int) -> Division {
+        let division = Division(code:code)
+        for _ in 0 ... size {
+            division.students.append(Student(forename: "Test", surname: "Student", birthday: Date())
+        }
+        return division
+    }
+    
+    static let examples = [Division.createDivision(code: "CMsiV-2", of: 10), Division.createDivision(code: "CComX-1", of: 8),Division.createDivision(code: "CGeoZ-1", of: 12), Division.createDivision(code: "CEngX-1", of: 11)]
+    
+    #endif
 }
