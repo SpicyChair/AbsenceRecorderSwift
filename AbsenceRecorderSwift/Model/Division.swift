118c0b8e0228c297f5044674b5226d3008747a24
Ethan <63197802+SpicyChair@users.noreply.github.com>
2022-01-23 18:16:51 +0000

    add student and division model

---
+++ AbsenceRecorderSwift/Model/Division.swift
@@ -0,0 +1,18 @@
+//
+//  Division.swift
+//  AbsenceRecorderSwift
+//
+//  Created by Hin, Ethan-Scott (WING) on 23/01/2022.
+//
+
+import Foundation
+
+
+class Division {
+    let code: String
+    let students:[String] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
---
+++ AbsenceRecorderSwift/Model/Student.swift
@@ -0,0 +1,21 @@
+//
+//  Student.swift
+//  AbsenceRecorderSwift
+//
+//  Created by Hin, Ethan-Scott (WING) on 23/01/2022.
+//
+
+import Foundation
+
+class Student {
+    let forename:String
+    let surname:String
+    let birthday:Date
+    
+    init(forename: String, surname:String, birthday:Date) {
+        self.birthday = birthday
+        self.forename = forename
+        self.surname = surname
+    }
+    
+}
